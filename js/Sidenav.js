258558fdda53040008ea972a7e2dd25460f32e15
Luis Linares <luis_linares75@hotmail.com>
2021-01-28 16:36:17 -0600

    Se añaden dos tipos de sidenav para abrir

--- js/Sidenav.js
+++ js/Sidenav.js
@@ -1,9 +1,33 @@
+var SIDENAV_FULL = 0;
+var SIDENAV_PUSH = 1;
+var BODY = document.getElementsByTagName('body').length > 0 ? document.getElementsByTagName('body')[0] : null;
+
 /* Open the sidenav */
-function openNav(sidenavName) {
-    document.getElementById(sidenavName).style.width = "100%";
+function openNav(sidenavName, sidenavType = SIDENAV_FULL) {
+    switch (sidenavType) {
+        case SIDENAV_PUSH:
+            //SIDENAV_PUSH
+            document.getElementById(sidenavName).style.width = "250px";
+            if(BODY != null){ BODY.style.marginLeft = "250px"; }
+            break;
+        default:
+            //SIDENAV_FULL
+            document.getElementById(sidenavName).style.width = "100%";
+            break;
+    }
 }
 
 /* Close/hide the sidenav */
-function closeNav(sidenavName) {
-    document.getElementById(sidenavName).style.width = "0";
+function closeNav(sidenavName, sidenavType = SIDENAV_FULL) {
+    switch (sidenavType) {
+        case SIDENAV_PUSH:
+            //SIDENAV_PUSH
+            document.getElementById(sidenavName).style.width = "0px";
+            if(BODY != null){ BODY.style.marginLeft = "0px"; }
+            break;
+        default:
+            //SIDENAV_FULL
+            document.getElementById(sidenavName).style.width = "0";       
+            break;
+    }
 }
