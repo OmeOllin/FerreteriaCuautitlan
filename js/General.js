e366bfebb3415333d12bc8d957540a5f4d674e73
OmeOllin <omeollincozcacuauhtli@gmail.com>
2021-02-18 21:22:52 -0600

    Se mejora legibilidad código

---
+++ js/General.js
@@ -0,0 +1,8 @@
+var xhrRequest = function (url, type, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        callback(this.responseText);
+    };
+    xhr.open(type, url);
+    xhr.send();
+};
